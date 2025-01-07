e8df629d3bd2436a0deee48b5227dca5ca9985d7
krysthecoder <krysthopher@gmail.com>
2025-01-06 21:40:01 -0600

    added daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="bg-white">
+      <button className="btn btn-secondary">secondary</button>
       <button className="btn btn-primary">primary</button>
-      <button className="btn btn-primary">primary</button>
-    </>
+    </div>
   );
 }
 
