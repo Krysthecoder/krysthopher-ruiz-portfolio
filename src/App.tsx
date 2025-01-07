f7649694a9536a23b2977a38ea337530f932f7a3
krysthecoder <krysthopher@gmail.com>
2025-01-06 21:32:33 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-primary">primary</button>
+      <button className="btn btn-primary">primary</button>
+    </>
+  );
+}
+
+export default App;
